10465c0229621577dc12371673e2c201b81afe78
dcalance <daniel.calancea@faf.utm.md>
2017-05-03 02:42:14 +0300

    Added lab 2

---
+++ lab_2/lab_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            double[][] arr = new double[][] { new double[] { 2, 3, 2, 1000 }, new double[] { 1, 1, 2, 800 }, new double[] {7, 8, 10, 0 } };
+            double[][] arr2 = new double[][] { new double[] { 0, 1200, 5800, 1500 }, new double[] { 1, 1, 1, 1.2 }, new double[] { -1, 0, 0, -0.5 }, new double[] { 8000, 16000, 24000, 0 } };
+            Simplex a = new Simplex(arr2);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ lab_2/lab_2/Simplex.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    class Simplex
+    {
+        double[][] simplexArray;
+        public Simplex(double[][] inputArr)
+        {
+            simplexArray = new double[inputArr.Length][];
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                simplexArray[i] = new double[inputArr[i].Length + inputArr.Length];
+                for (int counter = 0; counter < simplexArray[i].Length; counter++)
+                {
+                    if (counter > inputArr[i].Length - 2)
+                    {
+                        simplexArray[i][counter] = (i == counter - inputArr[i].Length + 1) ? 1 : 0;
+                    }
+                    else
+                    {
+                        simplexArray[i][counter] = inputArr[i][counter];
+                    }
+                }
+                simplexArray[i][simplexArray[i].Length - 1] = inputArr[i][inputArr[i].Length - 1];
+            }
+            for (int i = 0; i < inputArr[inputArr.Length - 1].Length; i++)
+            {
+                simplexArray[simplexArray.Length - 1][i] *= -1;
+            }
+            simplexArray[2][0] = 1;
+            simplexArray[2][5] = -1;
+            simplexArray[2][7] = 0.5;
+            solve();
+        }
+
+        void solve()
+        {
+            int[] finalArrIndexes = new int[simplexArray.Length - 1];
+            while (simplexArray[simplexArray.Length - 1].Min() < 0)
+            {
+                int minPivotIndex = 0;
+                double minPivotValue = simplexArray[simplexArray.Length - 1][0];
+
+                for (int i = 1; i < simplexArray[simplexArray.Length - 1].Length; i++)
+                {
+                    if (simplexArray[simplexArray.Length - 1][i] < minPivotValue)
+                    {
+                        minPivotValue = simplexArray[simplexArray.Length - 1][i];
+                        minPivotIndex = i;
+                    }
+                }
+
+                double minRowValue = simplexArray[0][minPivotIndex];
+                int minRowIndex = 0;
+
+                for (int i = 1; i < simplexArray.Length - 1; i++)
+                {
+                    if (simplexArray[i][minPivotIndex] != 0 && simplexArray[i][simplexArray[i].Length - 1] / simplexArray[i][minPivotIndex] < simplexArray[minRowIndex][simplexArray[minRowIndex].Length - 1] / minRowValue)
+                    {
+                        minRowValue = simplexArray[i][minPivotIndex];
+                        minRowIndex = i;
+                    }
+                }
+
+                for (int i = 0; i < simplexArray[minRowIndex].Length; i++)
+                {
+                    simplexArray[minRowIndex][i] /= minRowValue;
+                }
+                finalArrIndexes[minRowIndex] = minPivotIndex;
+
+                for (int rows = 0; rows < simplexArray.Length; rows++)
+                {
+                    double nr = simplexArray[rows][minPivotIndex];
+                    for (int cols = 0; cols < simplexArray[rows].Length; cols++)
+                    {
+                        if (rows != minRowIndex)
+                        {
+                            simplexArray[rows][cols] += simplexArray[minRowIndex][cols] * -nr;
+                        }
+                    }
+                }
+            }
+            double[] finalValues = new double[simplexArray[simplexArray.Length - 1].Length / 2];
+            for (int i = 0; i < finalArrIndexes.Length; i++)
+            {
+                finalValues[finalArrIndexes[i]] = simplexArray[i][simplexArray[i].Length - 1];
+            }
+        }
+    }
+}
